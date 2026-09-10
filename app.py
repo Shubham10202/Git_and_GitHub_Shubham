5ea9ce9f56011791d4a36b9c5f30fc7d65647c4a
Shubham10202 <pandeyshubham10202@gmail.com>
2026-09-10 19:53:22 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello from Shubham's Flask App!"
+
+@app.route("/api")
+def api():
+    with open("data.json", "r") as file:
+        data = json.load(file)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
